9d51ee89614282e5e5c85dfdbbc9f54b89e85dfa
Noveryan Putra <noveryanpp@gmail.com>
2025-02-04 14:58:54 +0700

    first commit

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,25 @@
+import { Image, StyleSheet, Platform } from "react-native";
+
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+
+export default function HomeScreen() {
+	return (
+		<>
+			<ThemedView>
+				<Image
+					source={require("@/assets/images/icon.svg")}
+					style={styles.logo}
+				/>
+			</ThemedView>
+		</>
+	);
+}
+
+const styles = StyleSheet.create({
+	logo: {
+		alignSelf: "center",
+	},
+});
