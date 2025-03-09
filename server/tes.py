42005203b0db18513f7c8228315ba8a412ae69f6
unknown <Noveryan Pamungkas>
2025-03-09 14:08:50 +0000

    Merge All Server-side Codes

---
+++ server/tes.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from dotenv import load_dotenv
+import mysql.connector
+import os
+from pathlib import Path
+
+dotenv_path = Path(".env")
+load_dotenv(dotenv_path=dotenv_path)
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
+
+# Koneksi ke Database
+def get_db_connection():
+    return mysql.connector.connect(
+        host=app.config['MYSQL_HOST'],
+        user=app.config['MYSQL_USER'],
+        password=app.config['MYSQL_PASSWORD'],
+        database=app.config['MYSQL_DB']
+    )
+    
+get_db_connection()
